f0999c2b8c05f6d1e1dc9c40178701185c344f75
thejengo <thejengo0o@gmail.com>
2020-10-02 21:27:05 +0300

    Added and implemented Domain.Core class library. Setup bus interface and command, message base classes.

---
+++ GameScope.Domain.Core/Commands/Command.cs
@@ -0,0 +1,15 @@
+﻿using GameScope.Domain.Core.Events;
+using System;
+
+namespace GameScope.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime TimeStamp { get; protected set; }
+
+        protected Command()
+        {
+            TimeStamp = DateTime.UtcNow;
+        }
+    }
+}
